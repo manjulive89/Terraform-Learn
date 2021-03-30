eaa7e3b5417f94cefca356db19157b8f34b2af5a
MAnjunath CR <manjulive89@gmail.com>
2021-03-31 02:53:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,16 @@ data "aws_subnet" "public" {
 
 resource "aws_instance" "test" {
   instance_type = "t2.micro"
+  count = 2
   ami = "ami-0915bcb5fa77e4892"
   security_groups = [ "sg-0263e6f2a528167b8" ]
  subnet_id = "subnet-06f478af15180f3b4"
+ user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
  tags = {
-   "Name" = "test"
+   "Name" = "Example Terraform"
  }
  }
